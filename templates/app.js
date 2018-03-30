86d18499e49ff0671e873c5502454d222cd7206a
ysera11 <hanyuzhu94@gmail.com>
2018-03-30 16:50:53 -0700

    update template to connect all four ms

--- templates/app.js
+++ templates/app.js
@@ -1,18 +1,16 @@
-module.exports = (bookings) => `
+module.exports = (amenities, bookings, similarListings, reviews) => `
   <section class="current-listing">
     <div class="current-listing-centered">
+      <section class="current-listing-info">
+        <div id="amenities">${amenities}</div>
+        <div id="reviews">${reviews}</div>
+      </section>
       <aside class="bookings-column">
         <div id="Bookings">${bookings}</div>
       </aside>
     </div>
   </section>
+  <section class="additional-content">
+    <div id="similar-listings">${similarListings}</div>
+  </section>
 `;
-
-// <section class="current-listing-info">
-//   <div id="amenities">${amenities}</div>
-//   <div id="reviews">${reviews}</div>
-// </section>
-
-// <section class="additional-content">
-//   <div id="similar-listings">${similarListings}</div>
-// </section>
